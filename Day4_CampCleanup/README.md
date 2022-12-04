b57b465a74742c95657047ef15e5d8894039a70a
Nicole Baldy <neb45@uakron.edu>
2022-12-04 11:05:46 -0500

    Part 2 - overlap

--- Day4_CampCleanup/README.md
+++ Day4_CampCleanup/README.md
@@ -9,3 +9,15 @@ input form "begin1-end1, begin2-end2"
 Regex to quickly pull out integers
 * True if `(begin1 <= begin2 && end1 >= end2)` (elf2 range completely contained in elf1 range)
 * True if `(begin2 <= begin1 && end2 >= end1)` (elf1 range completely contained in elf2 range)
+
+### Part 2
+Find any overlaps
+
+### Thoughts
+Basically the same strategy as last time, but it is true if
+* `(begin1 >= begin2 && begin1 <= end2)` (elf1 range starts within elf2 range)
+* `(end1 >= begin2 && end1 <= end2)` (elf1 range ends within elf2 range)
+Error: This does not catch elf2 range completely within elf1 range. Can add additional check, or reduce by checking the 'else' conditions:
+Given Elf1Range = [A, B], Elf2Range=[C, D]
+Overlap if order not A, B, C, D or C, D, A, B
+* `If not B < C or D < A`
--- Day4_CampCleanup/camp_cleanup_assignments.py
+++ Day4_CampCleanup/camp_cleanup_assignments.py
@@ -27,6 +27,21 @@ def isIsAssignmentRangeContained(line: str) -> bool:
         or (begin2 <= begin1 and end2 >= end1))
 
 
+def isIsAssignmentRangeOverlap(line: str) -> bool:
+    match = re.search("(\d*)-(\d*),(\d*)-(\d*)", line)
+
+    if not match:
+        print(f"Error, invalid line format: {line}.")
+        return False
+
+    begin1 = int(match.group(1))
+    end1 = int(match.group(2))
+    begin2 = int(match.group(3))
+    end2 = int(match.group(4))
+
+    return not (end1 < begin2 or end2 < begin1)
+
+
 def main(args: list):
     if len(args) != 1:
         print(f"Error: wrong number of arguments. Got {len(args)}, Exected 1.")
@@ -39,12 +54,16 @@ def main(args: list):
         print(f"{args[0]} does not exist")
         return 0
 
-    num_matches = 0
+    num_contained = 0
+    num_overlap = 0
     for line in file:
         if isIsAssignmentRangeContained(line):
-            num_matches += 1
+            num_contained += 1
+        if isIsAssignmentRangeOverlap(line):
+            num_overlap += 1
 
-    print(f"Advent of code answer part 1: {num_matches}")
+    print(f"Advent of code answer part 1: {num_contained}")
+    print(f"Advent of code answer part 2: {num_overlap}")
 
 
 if __name__ == '__main__':
