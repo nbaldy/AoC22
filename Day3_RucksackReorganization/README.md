3aad20676b804ebf31e7d3d4c6235b1c49d96057
Nicole Baldy <neb45@uakron.edu>
2022-12-03 23:37:32 -0500

    Complete part 2 - find badges

--- Day3_RucksackReorganization/README.md
+++ Day3_RucksackReorganization/README.md
@@ -11,3 +11,11 @@ Find the duplicate items (case sensitive) in each half of the rucksack and sum t
 At this stage, item priorities are not used to actually reorganize
 
 Seems simple enough, split the string into halves and bucket sort the halves (since we aren't reorganizing, this is faster than building a map, especially since we have continuous keys explicitly baked into the problem)
+
+### Part 2
+Between each set of 3 lines, one item will be common. Find the sum of these item priorities
+
+### Thoughts
+Change part 1 to keep a constant compartment for 3 lines at a time - return when an item is found in all 3 rucksacks
+
+However this requires changing the rucksack to allow different rucksacks instead of compartments
--- Day3_RucksackReorganization/rucksack_reorganize.py
+++ Day3_RucksackReorganization/rucksack_reorganize.py
@@ -10,27 +10,38 @@ import sys
 from enum import Enum
 import re
 
-class ItemLocation(Enum):
+class Compartments(Enum):
     EMPTY = 0
     COMPARTMENT_1 = 1
     COMPARTMENT_2 = 2
 
 class Rucksack():
     def __init__(self):
-        self.items = [ItemLocation.EMPTY] * 52
-    
+        self.items = [Compartments.EMPTY.value] * 52
+
     # Returns false if item already exists in the other component
-    def addItem(self, item : int, location : ItemLocation) -> bool:
+    def addItemToCompartment(self, item : int, location : Compartments) -> bool:
         if item < 1 or item > 52:
             print(f"Error: invalid item {item}; should be from 1-52")
             return False
 
-        if ItemLocation.EMPTY != self.items[item - 1]:
+        if Compartments.EMPTY != Compartments(self.items[item - 1]):
             return location == self.items[item - 1]
-        
+
         self.items[item - 1] = location
         return True
-    
+
+    # Maps the rucksack to a mask setup where rucksack 0 is binary 0, etc
+    # Keeps track of which sack has which item
+    def addItemToGroup(self, item : int, rucksack_num: int) -> int:
+        if item < 1 or item > 52:
+            print(f"Error: invalid item {item}; should be from 1-52")
+            return False
+
+        self.items[item - 1] |= 1 << rucksack_num
+
+        return self.items[item - 1]
+
     def __str__(self):
         return str(self.items)
 
@@ -40,7 +51,7 @@ def getItemPriority(item : str) -> int:
         return ord(item) - ord('a') + 1 # Lowercase starts at 1
     if ord('A') <= ord(item) and ord(item) <= ord('Z'):
         return ord(item) - ord('A') + 27 # Uppercase starts at 27
-    
+
     print(f"ERROR: invalid line item {item}")
     return 0
 
@@ -55,16 +66,16 @@ def findDuplicateRucksackItem(line: str) -> int:
     for i in range(len(line)//2):
         item_c1 = getItemPriority(line[i])
         item_c2 = getItemPriority(line[-1-i])
-        
-        if not rucksack.addItem(item_c1, ItemLocation.COMPARTMENT_1):
+
+        if not rucksack.addItemToCompartment(item_c1, Compartments.COMPARTMENT_1):
             return  item_c1
 
-        if not rucksack.addItem(item_c2, ItemLocation.COMPARTMENT_2):
+        if not rucksack.addItemToCompartment(item_c2, Compartments.COMPARTMENT_2):
             return  item_c2
-        
+
     print(f"\nNo duplicates found: {rucksack}")
     return 0
-            
+
 def getDuplicateValueSum(filename : str) -> int:
     try:
         file = open(filename, 'r')
@@ -75,9 +86,49 @@ def getDuplicateValueSum(filename : str) -> int:
     sum_duplicates = 0
     for line in file:
         sum_duplicates += findDuplicateRucksackItem(line)
-    
+
     return sum_duplicates
-    
+
+def getBadgeValueSum(filename : str) -> int:
+    try:
+        file = open(filename, 'r')
+    except FileNotFoundError:
+        print(f"{filename} does not exist")
+        return 0
+
+    sum_badges = 0
+
+    while True:
+        group_rucksacks = Rucksack()
+
+        # First elf
+        elf1_line = file.readline().strip()
+        elf2_line = file.readline().strip()
+        elf3_line = file.readline().strip()
+        if not elf1_line:
+            # Done processing - assume sets of 3 but if not process as if remainders are empty
+            break
+
+        # Technically if we *really* cared, we should probably process the sacks round-robin style
+        # As this reduces our omega bound (because theoretically if all of the first elements are duplicates,
+        # the check occurs in constant time (check the first element of each elf) rather than linear time
+        for item in elf1_line:
+            # Don't care about result - not enough info yet
+            _ = group_rucksacks.addItemToGroup(getItemPriority(item), 0)
+        for item in elf2_line:
+            # Don't care about result - not enough info yet
+            _ = group_rucksacks.addItemToGroup(getItemPriority(item), 1)
+        for item in elf3_line:
+            item_val = getItemPriority(item)
+            item_compartments = group_rucksacks.addItemToGroup(item_val, 2)
+            if  0b111 == item_compartments:
+                # Present in all 3 compartments
+                sum_badges += item_val
+                break
+
+    return sum_badges
+
+
 
 def main(args: list):
     if len(args) != 1:
@@ -86,7 +137,8 @@ def main(args: list):
         return
 
     sum_duplicates = getDuplicateValueSum(args[0])
-    print(f"Advent of code answer part 1: {sum_duplicates}")
+    print(f"Advent of code answer part 1: {getDuplicateValueSum(args[0])}")
+    print(f"Advent of code answer part 2: {getBadgeValueSum(args[0])}")
 
 if __name__ == '__main__':
     main(sys.argv[1:])
